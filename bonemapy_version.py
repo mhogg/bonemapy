bab9464394c91f63786c50080292347339aa122a
Michael Hogg <michael.christopher.hogg@gmail.com>
2013-09-25 09:51:17 +1000

    Add license + redist info to version file

--- bonemapy_version.py
+++ bonemapy_version.py
@@ -1,2 +1,7 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013 Michael Hogg
+
+# This file is part of bonemapy - See LICENSE.txt for information on usage and redistribution
 
 __version__ = '0.2.2'
