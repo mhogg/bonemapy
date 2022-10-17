fa0236872bf6d7efb11dc7ee3129420b61ac9a36
Michael Hogg <michael.hogg@resmed.com.au>
2022-10-17 20:55:36 +1100

    Bump version to 0.6.2

--- bonemapy/bonemapy_version.py
+++ bonemapy/bonemapy_version.py
@@ -5,5 +5,5 @@
 # This file is part of bonemapy - See LICENSE.txt for information on usage and redistribution
 
 # Version and date
-version     = '0.6.1'
+version     = '0.6.2'
 releasedate = '2022-10-17'
